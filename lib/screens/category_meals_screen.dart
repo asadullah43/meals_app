4508ee9d475d859ac8b3697cae0f5c82c0b28efd
asadUllah43 <asadu043@gmail.com>
2024-06-18 23:09:09 +0300

    add meals screen and pic

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = 'category-meals';
+  // final String categoryid;
+  // final Color? color;
+  // final String categorytitle;
+  const CategoryMealsScreen({
+    super.key,
+    // required this.categoryid,
+    // this.color,
+    // required this.categorytitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle!),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                complexity: categoryMeals[index].complexity,
+                affordability: categoryMeals[index].affordability);
+          },
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
